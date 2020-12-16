b11432752a0c3f413ba5fa80069aca54093f6c44
GitHub Action <action@github.com>
2020-12-16 16:08:03 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/musketyr/exampleintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.musketyr.exampleintellijplugin.services
+
+import com.github.musketyr.exampleintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
